20a9a38aae953245bc36845e26702eaa4e835f68
stianStensli <stian.hs@hotmail.com>
2018-04-11 09:41:26 +0200

    need to look over all comp whit double

--- src/main/java/com/oblig5/transaction/controller/TransactionController.java
+++ src/main/java/com/oblig5/transaction/controller/TransactionController.java
@@ -9,6 +9,7 @@ import com.oblig5.transaction.service.TransactionService;
 import com.oblig5.transaction.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -22,6 +23,7 @@ public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
+
 
     @RequestMapping("buyBTC")
     public String buy(Principal principal, Double price, Double amount, RedirectAttributes redirectAttrs){
